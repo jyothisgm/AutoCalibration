Fixed phantom rotation.

bead_detection.py
@@ -249,11 +249,11 @@ def build_wide_df_from_folder(folder: str, K: int = 5, min_area: int = 10, max_a
     return proj_data
 
 if __name__ == "__main__":
-    folder = f"2026-02-19_Beads_phantom/Scan11/out_line_integrals"
-    # folder = "projections_png_reakl"
+    folder = f"real_scans/2026-02-19_Beads_phantom/Scan1/out_line_integrals"
+    folder = f"fake_projections/test"
     K = 5
 
-    df = build_wide_df_from_folder(folder, K=K, min_area=10, max_area=2000, exclude_border=0, connectivity=8, file_type=".tif", tolerance=130)
+    df = build_wide_df_from_folder(folder, K=K, min_area=10, max_area=2000, exclude_border=0, connectivity=8, file_type=".png", tolerance=130)
     numeric_cols = df.select_dtypes(include=np.number)
 
     # Compute range (max - min)
@@ -265,4 +265,4 @@ if __name__ == "__main__":
     range_row["image"] = "range"
     df.loc[len(df)] = range_row
 
-    print(df)
+    df.to_csv(os.path.join(folder, "bead_detections.csv"), index=False)

extras/napori.py
@@ -2,16 +2,48 @@ import napari
 from imageio.v3 import imread, imwrite
 import numpy as np
 from glob import glob
+import matplotlib.pyplot as plt
 
-recon = np.stack([imread(im) for im in sorted(glob('C:\\Users\\Flex Ray\\Documents\\JGM\\AutoCalibration\\2026-02-19_Beads_phantom\\Scan3\\out_line_integrals\\*.tif'))])
-
-print(recon.shape)
-napari.imshow(recon, axis_labels=("z", "y", "x"))
-napari.run()
-
-recon = np.stack([imread(im) for im in sorted(glob('C:\\Users\\Flex Ray\\Documents\\JGM\\AutoCalibration\\2026-02-09_Beads_phantom\\Scan1\\cropped\\*.tif'))])
+# recon = np.stack([imread(im) for im in sorted(glob('C:\\Users\\Flex Ray\\Documents\\JGM\\AutoCalibration\\real_scans\\2026-02-19_Beads_phantom\\Scan1\\out_line_integrals\\*.tif'))])
+recon = np.stack([imread(im) for im in sorted(glob(f'C:\\Users\\Flex Ray\\Documents\\JGM\\AutoCalibration\\fake_projections\\test\\*.png'))])
 
 print(recon.shape)
-napari.imshow(recon, axis_labels=("z", "y", "x"))
+napari.imshow(recon)
 napari.run()
 
+# recon = np.stack([imread(im) for im in sorted(glob('C:\\Users\\Flex Ray\\Documents\\JGM\\AutoCalibration\\real_scans\\2026-02-09_Beads_phantom\\Scan1\\cropped\\*.tif'))])
+
+# print(recon.shape)
+# napari.imshow(recon, axis_labels=("y", "z", "x"))
+# napari.run()
+
+# volume = np.load(r"C:\Users\Flex Ray\Documents\JGM\AutoCalibration\phantoms\scan2_160x240x498.npy")
+# z, y, x = volume.shape
+# print(f"Volume shape: x={x}, y={y}, z={z}")
+# plt.figure(figsize=(12,4))
+
+# plt.subplot(1,3,1)
+# axis_limits = (0, 600)
+
+# plt.imshow(volume[28], cmap="gray")
+# plt.xlim(axis_limits)
+# plt.ylim(axis_limits)
+
+# plt.title("Axial")
+
+# plt.subplot(1,3,2)
+# plt.imshow(volume[:, 20, :], cmap="gray")
+# plt.xlim(axis_limits)
+# plt.ylim(axis_limits)
+# plt.title("Coronal")
+
+# plt.subplot(1,3,3)
+# plt.imshow(volume[:, :, 30], cmap="gray")
+# plt.xlim(axis_limits)
+# plt.ylim(axis_limits)
+# plt.title("Sagittal")
+
+# plt.tight_layout()
+# plt.show()
+
+# print()

gauss_newton.py
@@ -127,20 +127,20 @@ def apply_theta_to_geometry(theta, src_world, obj_world, det_world, obj_rot_y_de
     return src_w, obj_w, det_w, rot_y
 
 def geom12_with_theta(theta, base_src_world, base_obj_world, base_det_world, base_rot_y_deg, 
-                      astra_scaling, det_spacing, src_up, src_right):
+                      astra_scaling, det_spacing, det_col, det_row):
     theta = np.asarray(theta, dtype=np.float64).reshape(9)
     dSx, dSy = theta[0], theta[1]
     dOx, dOy, dOz = theta[2], theta[3], theta[4]
     dDx, dDy, dDz = theta[5], theta[6], theta[7]
     alpha = theta[8]  # degrees
 
-    src_world = base_src_world + dSx * src_right + dSy * src_up
+    src_world = base_src_world + dSx * det_row + dSy * det_col
     obj_world = base_obj_world + np.array([dOx, dOy, dOz], dtype=np.float64)
     det_world = base_det_world + np.array([dDx, dDy, dDz], dtype=np.float64)
     rot_deg = float(base_rot_y_deg + alpha)
 
     return unity_geom12_from_worldcoords(src_world=src_world, obj_world=obj_world, det_world=det_world, obj_rot_y_deg=rot_deg,
-                                         astra_scaling=astra_scaling, det_spacing=det_spacing, src_up=src_up, src_right=src_right)
+                                         astra_scaling=astra_scaling, det_spacing=det_spacing, det_col=det_col, det_row=det_row)
 
 def project_points_cone_vec(geom12: np.ndarray, bead_xyz: np.ndarray, det_h: int, det_w: int):
     g = np.asarray(geom12, dtype=np.float64).reshape(12)
@@ -226,8 +226,8 @@ def build_residual(theta, df_wide, bead_xyz, angles_deg, cfg):
             base_rot_y_deg=float(angles_deg[i]),
             astra_scaling=cfg["astra_scaling"],
             det_spacing=cfg["DET_SPACING"],
-            src_up=cfg["SRC_UP"],
-            src_right=cfg["SRC_RIGHT"],
+            det_col=cfg["DET_COL"],
+            det_row=cfg["DET_ROW"],
         )
 
         pred = project_points_cone_vec(geom12, bead_xyz, det_h, det_w)
@@ -298,8 +298,8 @@ def generate_predicted_projections(theta, angles_deg, cfg, out_dir):
         astra_scaling=cfg["astra_scaling"],
         det_spacing=cfg["DET_SPACING"],
         voxel_size=cfg["VOXEL_SIZE"],
-        src_up=cfg["SRC_UP"],
-        src_right=cfg["SRC_RIGHT"],
+        det_col=cfg["DET_COL"],
+        det_row=cfg["DET_ROW"],
         filename_prefix="proj",
         #phantom_name=HERE/f"phantoms/cuboid_phantom_{cfg['K']}.npy"
         phantom_name=HERE/f"phantoms/recon_cropped_scan_1.npy"
@@ -382,8 +382,8 @@ def lm_solve_image_based(real_df, angles_deg, cfg, n_iters=10, lam=1e-2, fix_sou
             obj_rot_y_deg=rot_y,
             astra_scaling=cfg["astra_scaling"],
             det_spacing=cfg["DET_SPACING"],
-            src_up=cfg["SRC_UP"],
-            src_right=cfg["SRC_RIGHT"],
+            det_col=cfg["DET_COL"],
+            det_row=cfg["DET_ROW"],
         )
         #print_geometry_vector(geom12)
 
@@ -469,8 +469,8 @@ def main():
 
     # xraySource orientation (world). Use your real values if different.
     # If your source GameObject has default rotation, these are usually:
-    SRC_UP = np.array([1.0, 0.0, 0.0], dtype=np.float32)
-    SRC_RIGHT = np.array([0.0, 1.0, 0.0], dtype=np.float32)
+    DET_COL = np.array([1.0, 0.0, 0.0], dtype=np.float32)
+    DET_ROW = np.array([0.0, 1.0, 0.0], dtype=np.float32)
 
 
     GEOM_SCENARIOS = [
@@ -601,8 +601,8 @@ def main():
                     astra_scaling=astra_scaling,
                     det_spacing=DET_SPACING,
                     voxel_size=VOXEL_SIZE,
-                    src_up=SRC_UP,
-                    src_right=SRC_RIGHT,
+                    det_col=DET_COL,
+                    det_row=DET_ROW,
                     filename_prefix="proj",
                     phantom_name=HERE/f"phantoms/cuboid_phantom_{each_k}.npy"
                     # phantom_name=HERE/f"phantoms/recon_cropped_scan_1.npy"
@@ -630,8 +630,8 @@ def main():
                     "OBJ_WORLD": UNITY_OBJ_WORLD,
                     "DET_WORLD": DET_WORLD,
                     "VOXEL_SIZE": VOXEL_SIZE,
-                    "SRC_UP": SRC_UP,
-                    "SRC_RIGHT": SRC_RIGHT,
+                    "DET_COL": DET_COL,
+                    "DET_ROW": DET_ROW,
                     "min_area": MIN_AREA,
                     "max_area": MAX_AREA,
                 }

gauss_newton_real.py
@@ -63,7 +63,7 @@ def print_delta_table(delta, iteration):
     print("delta offsets (Unity frame):")
     print(f"  Source   : dSx={dSx:8.3f}, dSy={dSy:8.3f}, dSz={0.0:8.3f}  (mm)")
     print(f"  Object   : dOx={dOx:8.3f}, dOy={dOy:8.3f}, dOz={dOz:8.3f}  (mm)")
-    print(f"  Object offset: offset_x={offset_x:8.3f}, offset_z={offset_z:8.3f} (mm)")
+    print(f"  Object offset: offset_x={offset_x:8.3f}, offset_z={offset_z:8.3f}  (mm)")
     print(f"  Detector : dDx={dDx:8.3f}, dDy={dDy:8.3f}, dDz={dDz:8.3f}  (mm)")
     print(f"  Obj Stage rotY : {alpha:8.3f} deg")
     print("=" * 60)
@@ -185,7 +185,8 @@ def residual_from_two_dfs(real_df, pred_df, K, area_weight: float = 1e-3, distan
 
     return r_vec, col_names
 
-def generate_predicted_projections(delta, angles_deg, cfg, out_dir):
+def build_residual_image_based(delta, real_df, angles_deg, cfg, pred_dir, debug=True):
+    # 1) generate predicted projections for current delta
     src_w, obj_w, det_w, alpha, offset_x, offset_z = apply_delta_to_geometry(
         delta=delta,
         src_world=cfg["SRC_WORLD"],
@@ -193,7 +194,7 @@ def generate_predicted_projections(delta, angles_deg, cfg, out_dir):
         det_world=cfg["DET_WORLD"],
     )
     fetch_and_save_projections(
-        out_dir=out_dir,
+        out_dir=pred_dir,
         src_world=src_w,
         obj_world=obj_w,
         det_world_base=det_w,
@@ -206,17 +207,13 @@ def generate_predicted_projections(delta, angles_deg, cfg, out_dir):
         astra_scaling=cfg["astra_scaling"],
         det_spacing=cfg["DET_SPACING"],
         voxel_size=cfg["VOXEL_SIZE"],
-        src_up=cfg["SRC_UP"],
-        src_right=cfg["SRC_RIGHT"],
+        det_col=cfg["DET_COL"],
+        det_row=cfg["DET_ROW"],
         filename_prefix="proj",
         #phantom_name=HERE/f"phantoms/cuboid_phantom_{cfg['K']}.npy"
-        phantom_name=HERE/f"phantoms/scan2_160x240x498.npy"
-        
+        phantom_name=HERE/f"phantoms/scan2_160x240x498_transposed_rotY180.npy",
+        debug=debug
     )
-
-def build_residual_image_based(delta, real_df, angles_deg, cfg, pred_dir):
-    # 1) generate predicted projections for current delta
-    generate_predicted_projections(delta, angles_deg, cfg, pred_dir)
 
     # 2) detect beads in predicted projections
     pred_df = build_wide_df_from_folder(
@@ -240,7 +237,7 @@ def build_residual_image_based(delta, real_df, angles_deg, cfg, pred_dir):
     return residual_from_two_dfs(real_df, pred_df, cfg["K"])
 
 def numerical_jacobian_image_based(delta, active_mask, real_df, angles_deg, cfg, eps, work_dir):
-    r0, cols = build_residual_image_based(delta, real_df, angles_deg, cfg, work_dir / "pred_base")
+    r0, cols = build_residual_image_based(delta, real_df, angles_deg, cfg, work_dir / "pred_base", debug=True)
 
     if len(r0) == 0:
         return None, None
@@ -254,8 +251,8 @@ def numerical_jacobian_image_based(delta, active_mask, real_df, angles_deg, cfg,
         t_m = delta.copy()
         t_p[j] += eps[j]; t_m[j] -= eps[j]
 
-        r_p, _ = build_residual_image_based(t_p, real_df, angles_deg, cfg, work_dir / f"pred_p_{j:02d}")
-        r_m, _ = build_residual_image_based(t_m, real_df, angles_deg, cfg, work_dir / f"pred_m_{j:02d}")
+        r_p, _ = build_residual_image_based(t_p, real_df, angles_deg, cfg, work_dir / f"pred_p_{j:02d}", debug=False)
+        r_m, _ = build_residual_image_based(t_m, real_df, angles_deg, cfg, work_dir / f"pred_m_{j:02d}", debug=False)
         if  len(r_p) == 0 or len(r_m) == 0:
             continue
 
@@ -279,7 +276,7 @@ def lm_solve_image_based(real_df, angles_deg, cfg, n_iters=10, lam=1e-2, fix_sou
         print_delta_table(delta, it)
 
         # ---- PRINT UNITY + ASTRA GEOM FOR FIRST VIEW ----
-        src_w, obj_w, det_w, alpha, offset_x, offset_z = apply_delta_to_geometry(delta, 
+        src_w, obj_w, det_w, _, _, _ = apply_delta_to_geometry(delta, 
             src_world=cfg["SRC_WORLD"],
             obj_world=cfg["OBJ_WORLD"],
             det_world=cfg["DET_WORLD"],
@@ -318,7 +315,8 @@ def lm_solve_image_based(real_df, angles_deg, cfg, n_iters=10, lam=1e-2, fix_sou
         df_iter.insert(2, "cost", [cost, cost_new])
         if df_r0 is None:
             df_r0 = pd.DataFrame(columns=["iter", "state", "cost"] + cols)
-        df_r0 = pd.concat([df_r0, df_iter], ignore_index=True)
+        df_r0 = pd.concat([df_r0, df_iter], ignore_index=True) 
+        df_r0.to_csv(os.path.join(work_dir, "residual_history.csv"), index=False)
 
         print(f"\niter {it:02d} cost={cost:.6f} -> {cost_new:.6f} |ddelta|={np.linalg.norm(ddelta):.6e}  lambda={lam:.3e}")
 
@@ -503,8 +501,8 @@ if __name__ == "__main__":
     
     astra_scaling = 1
 
-    SRC_UP = np.array([1.0, 0.0, 0.0], dtype=np.float32)
-    SRC_RIGHT = np.array([0.0, 1.0, 0.0], dtype=np.float32)
+    DET_ROW = np.array([0.0, 1.0, 0.0], dtype=np.float32)
+    DET_COL = np.array([1.0, 0.0, 0.0], dtype=np.float32)
 
     BEAD_COUNT = K = 5
     ANGLE_FACTORS = [3, 4, 5, 6, 8, 9, 10, 12, 15, 18, 20, 24, 30, 36, 40, 45, 60, 72, 90, 120, 180, 360]
@@ -514,7 +512,7 @@ if __name__ == "__main__":
     AREA_WEIGHT = 1e-3
     VOXEL_SIZE = 0.1
 
-    PHANTOM_PATH = HERE / "phantoms/scan2_160x240x498.npy"
+    PHANTOM_PATH = HERE / "phantoms/scan2_160x240x498_transposed_rotY180.npy"
     BASE_REAL_DIR = HERE / "real_scans/2026-02-19_Beads_phantom"
 
     used_projections = [360]
@@ -557,8 +555,8 @@ if __name__ == "__main__":
                 "OBJ_WORLD": sc["obj"],
                 "DET_WORLD": sc["det"],
                 "VOXEL_SIZE": VOXEL_SIZE,
-                "SRC_UP": SRC_UP,
-                "SRC_RIGHT": SRC_RIGHT,
+                "DET_COL": DET_COL,
+                "DET_ROW": DET_ROW,
                 "min_area": MIN_AREA,
                 "max_area": MAX_AREA,
                 "box_images": True,

phantom_projector.py
@@ -6,6 +6,7 @@ import numpy as np
 
 from PIL import Image
 from astra_server import AstraServer
+from extras.scan_settings import write_scan_settings_txt
 
 
 def reset_folder(folder):
@@ -45,6 +46,32 @@ def apply_napari_contrast_and_gamma(
 
     return out
 
+def geom12_metrics(g):
+    g = np.asarray(g, dtype=float).reshape(12)
+
+    # unpack from ASTRA packed (x,z,y) back to true (x,y,z)
+    S = unpack_xzy(g[0:3])
+    D = unpack_xzy(g[3:6])
+    u = unpack_xzy(g[6:9])
+    v = unpack_xzy(g[9:12])
+
+    sod = np.linalg.norm(S)
+    sdd = np.linalg.norm(D - S)
+    mag = sdd / sod
+
+    r = D - S  # source -> detector
+    r_hat = r / np.linalg.norm(r)
+
+    n = np.cross(u, v)
+    n_hat = n / np.linalg.norm(n)
+
+    # Physical incidence: should be 0° whether normal is + or - (plane has two normals)
+    cosang = float(np.dot(r_hat, n_hat))
+    cosang = np.clip(cosang, -1.0, 1.0)
+    inc_deg = float(np.degrees(np.arccos(abs(cosang))))
+
+    return sod, sdd, mag, inc_deg
+
 def rotate_y_xz(v: np.ndarray, cosY: float, sinY: float) -> np.ndarray:
     """
     Unity rotation around Y axis applied to x,z (y unchanged):
@@ -57,7 +84,6 @@ def rotate_y_xz(v: np.ndarray, cosY: float, sinY: float) -> np.ndarray:
     return np.array([x2, y, z2], dtype=np.float32)
 
 def pack_xzy(v: np.ndarray) -> np.ndarray:
-    # Unity packing used in your C# client: (x, z, y)
     return np.array([v[0], v[2], v[1]], dtype=np.float32)
 
 def unpack_xzy(v: np.ndarray) -> np.ndarray:
@@ -87,50 +113,34 @@ def unity_geom12_from_worldcoords(
     src_world: np.ndarray,     # xraySource.position
     obj_world: np.ndarray,     # imagedObject.position
     det_world: np.ndarray,     # detObject.position
+    initial_calibration: np.ndarray, # (3, 3) array of initial offsets to apply to src_world, obj_world, det_world respectively, to match Astra's geometry convention. Each row is a (x,y,z) offset.
     obj_rot_y_deg: float,      # imagedObject.rotation.eulerAngles.y
     alpha: float,               # initial angle offset (degrees) to apply to obj_rot_y_deg, if needed to match Astra's angle=0 convention
     astra_scaling: float,      # astra scaling factor to convert from Unity units to mm (e.g. 1.0 if 1 unit = 1 mm)f
     det_spacing: float,        # astraDetectorSpacing
-    src_up: np.ndarray,        # xraySource.up
-    src_right: np.ndarray,     # xraySource.right
+    det_col: np.ndarray,        # xraySource.up
+    det_row: np.ndarray,     # xraySource.right
     offset_x: float = 0.0,          # Optional additional offset in x (Unity right) direction, applied after rotation
     offset_z: float = 0.0           # Optional additional offset in z (Unity forward) direction, applied after rotation
 ) -> np.ndarray:
-    # intial_calibration = np.array([
-    #     np.array([0.0, 0.0, -14.9192], dtype=np.float32),
-    #     np.array([-0.540527, 0.0, 0.0], dtype=np.float32), 
-    #     np.array([25.318359, 6.31, 25.081600], dtype=np.float32)
-    # ])
-    intial_calibration = np.array([
-        np.array([0.0, 0.0, 0], dtype=np.float32),
-        np.array([0, 0.0, 0.0], dtype=np.float32),
-        np.array([25.318359, 6.31, 40.00000], dtype=np.float32)
-    ])
-    
-    # intial_calibration = np.array([
-    #     np.array([0.0, 0.0, 0.0], dtype=np.float32),
-    #     np.array([0.0, 0.0, 0.0], dtype=np.float32),
-    #     np.array([0.0, 0.0, 0.0], dtype=np.float32),
-    # ])
-    
     obj_rot = np.deg2rad(float(obj_rot_y_deg))
     alpha = np.deg2rad(float(alpha))
     offset_x_rot = offset_x * np.cos(obj_rot + alpha) - offset_z * np.sin(obj_rot + alpha)
     offset_z_rot = offset_x * np.sin(obj_rot + alpha) + offset_z * np.cos(obj_rot + alpha)
 
-    src_world = np.asarray(src_world, dtype=np.float32) + intial_calibration[0]
-    obj_world = np.asarray(obj_world, dtype=np.float32) + intial_calibration[1] + np.array([offset_x_rot, 0.0, offset_z_rot], dtype=np.float32)
-    det_world = np.asarray(det_world, dtype=np.float32) + intial_calibration[2]
-    src_up = np.asarray(src_up, dtype=np.float32)
-    src_right = np.asarray(src_right, dtype=np.float32)
+    src_world = np.asarray(src_world, dtype=np.float32) + initial_calibration[0]
+    obj_world = np.asarray(obj_world, dtype=np.float32) + initial_calibration[1] + np.array([offset_x_rot, 0.0, offset_z_rot], dtype=np.float32)
+    det_world = np.asarray(det_world, dtype=np.float32) + initial_calibration[2]
+    det_col = np.asarray(det_col, dtype=np.float32)
+    det_row = np.asarray(det_row, dtype=np.float32)
 
     # srcPos / detPos in "object space" (relative to object) and then scaled by SDD
     srcPos = (src_world - obj_world) * astra_scaling
     detPos = (det_world - obj_world) * astra_scaling
 
     # u and v from source basis
-    u = src_up * det_spacing
-    v = src_right * det_spacing
+    u = det_col * det_spacing
+    v = det_row * det_spacing
 
     # Rotate around Y by object rotation
     cosY = float(np.cos(obj_rot + alpha))
@@ -149,22 +159,42 @@ def fetch_and_save_projections(out_dir: str, src_world: np.ndarray, obj_world: n
                                alpha: float, angles_deg: np.ndarray, offset_x: float, offset_z: float,
                                image_height: int, image_width: int,
                                astra_scaling: float, det_spacing: float, voxel_size: float,
-                               src_up: np.ndarray, src_right: np.ndarray, filename_prefix: str = "proj", phantom_name: str = "cuboid_phantom.npy"):
+                               det_col: np.ndarray, det_row: np.ndarray, filename_prefix: str = "proj", phantom_name: str = "cuboid_phantom.npy", debug=True):
     reset_folder(out_dir)
     rec = np.load(phantom_name)
 
-    server = AstraServer(object=rec, image_width=image_width, image_height=image_height, voxel_size=voxel_size)
-    # print_unity_geometry(src_world, obj_world, det_world_base, obj_rot_y_degs[0])
-    # for idx, ry in enumerate(obj_rot_y_degs):
-    #     geom12 = unity_geom12_from_worldcoords(src_world=src_world, obj_world=obj_world, det_world=det_world_base,
-    #                                            obj_rot_y_deg=float(ry), astra_scaling=astra_scaling, det_spacing=det_spacing,
-    #                                            src_up=src_up, src_right=src_right)
-    #     # np.set_printoptions(precision=3, suppress=True)
-    #     img = server.generate_image(geom12)
-    #     #Image.fromarray(img).save(os.path.join(out_dir, f"{filename_prefix}_{int(ry):03d}_{idx:03d}.png"))
+    # intial_calibration = np.array([
+    #     np.array([0.0, 0.0, -14.9192], dtype=np.float32),
+    #     np.array([-0.540527, 0.0, 0.0], dtype=np.float32), 
+    #     np.array([25.318359, 6.31, 25.081600], dtype=np.float32)
+    # ])
+    initial_calibration = np.array([
+        np.array([ 0.0     , 0.00, 00.00000], dtype=np.float32),
+        np.array([-0.540527, 0.00, 14.91920], dtype=np.float32),
+        np.array([25.318359, 6.31, 40.00080], dtype=np.float32)
+    ])
+    
+    # intial_calibration = np.array([
+    #     np.array([0.0, 0.0, 0.0], dtype=np.float32),
+    #     np.array([0.0, 0.0, 0.0], dtype=np.float32),
+    #     np.array([0.0, 0.0, 0.0], dtype=np.float32),
+    # ])
+    if debug:
+        write_scan_settings_txt(
+            out_dir=out_dir,
+            image_width=image_width,
+            image_height=image_height,
+            voxel_size=voxel_size,
+            det_spacing=det_spacing,
+            src_world=src_world,
+            obj_world=obj_world,
+            det_world=det_world_base,
+            initial_calibration=initial_calibration,
+            astra_scaling=astra_scaling,
+            angles_deg=angles_deg,
+        )
 
-    #     img = apply_napari_contrast_and_gamma(img, low_percentile=99.5, high_percentile=100.0, gamma=0.2)
-    #     Image.fromarray(img).save(os.path.join(out_dir, f"{filename_prefix}_{idx:03d}.png"))
+    server = AstraServer(object=rec, image_width=image_width, image_height=image_height, voxel_size=voxel_size)
     geom12_array = []
 
     # 1) Build all geometries first
@@ -173,19 +203,27 @@ def fetch_and_save_projections(out_dir: str, src_world: np.ndarray, obj_world: n
             src_world=src_world,
             obj_world=obj_world,
             det_world=det_world_base,
+            initial_calibration=initial_calibration,
             obj_rot_y_deg=float(ry),
             alpha=float(alpha),
             astra_scaling=astra_scaling,
             det_spacing=det_spacing,
-            src_up=src_up,
-            src_right=src_right,
+            det_col=det_col,
+            det_row=det_row,
             offset_x=offset_x,
             offset_z=offset_z
         )
         geom12_array.append(geom12)
 
     geom12_array = np.asarray(geom12_array, dtype=np.float32)  # (N, 12)
-
+    
+    if debug:
+        sod, sdd, mag, inc_deg = geom12_metrics(geom12_array[0])
+        print(
+            f"view {0:3d} | SOD={sod:8.3f} mm | "
+            f"SDD={sdd:8.3f} mm | M={mag:6.3f} | "
+            f"incident={inc_deg:6.4f}°"
+        )
     # 2) Generate all projections in one call (server.generate_images from earlier)
     imgs = server.generate_stacked_images(geom12_array)  # (N, H, W, 3)
 
@@ -200,34 +238,33 @@ def fetch_and_save_projections(out_dir: str, src_world: np.ndarray, obj_world: n
     # print(f"Saved {len(obj_rot_y_degs)} images to: {out_dir}")
 
 
-def main():
+if __name__ == "__main__":
     # Must match server.py
     IMAGE_W = 956
     IMAGE_H = 760
 
     # Must match Unity settings
     astra_scaling = 1
-    DET_SPACING = 0.075
+    DET_SPACING = 0.149600
 
     # ---- Unity world coordinates you mentioned ----
     # source default position
-    SRC_WORLD = np.array([0.0, 45.0, 0.0], dtype=np.float32)
+    SRC_WORLD = np.array([ 0.      , 24.997368,  0.      ], dtype=np.float32)
 
     # object position
-    OBJ_WORLD = np.array([0.0, 30.0, 570.0], dtype=np.float32)
+    OBJ_WORLD = np.array([  0.540527, 22.49 , 600.      ], dtype=np.float32)
 
     # detector position (world)
-    DET_WORLD = np.array([0.0, 0.0, 1004.0], dtype=np.float32)
+    DET_WORLD = np.array([ -25.31836 ,   18.686905, 1059.      ], dtype=np.float32)
     VOXEL_SIZE = 0.1
 
     # xraySource orientation (world). Use your real values if different.
     # If your source GameObject has default rotation, these are usually:
-    SRC_RIGHT = np.array([0.0, 1.0, 0.0], dtype=np.float32)
-    SRC_UP = np.array([1.0, 0.0, 0.0], dtype=np.float32)
+    DET_ROW = np.array([0.0, 1.0, 0.0], dtype=np.float32)
+    DET_COL = np.array([1.0, 0.0, 0.0], dtype=np.float32)
 
     OUT_DIR = f"fake_projections/test"
-    PHANTOM_NAME = f"phantoms/cuboid_phantom.npy"  # Must be a 3D numpy array representing the phantom volume. Adjust path if needed.
-    PHANTOM_NAME = f"phantoms/recon_cropped_scan_1.npy"  # Must be a 3D numpy array representing the phantom volume. Adjust path if needed.
+    PHANTOM_NAME = f"phantoms/scan2_160x240x498_transposed_rotY180.npy"  # Must be a 3D numpy array representing the phantom volume. Adjust path if needed.
 
     # Number of angles: we emulate imagedObject.rotation.eulerAngles.y
     N_ANGLES = 360
@@ -238,14 +275,14 @@ def main():
         src_world=SRC_WORLD,
         obj_world=OBJ_WORLD,
         det_world_base=DET_WORLD,
-        obj_rot_y_degs=obj_rot_y_degs,
+        alpha= 0.0, angles_deg=obj_rot_y_degs, offset_x=0, offset_z=0,
         image_height=IMAGE_H,
         image_width=IMAGE_W,
         astra_scaling=astra_scaling,
         det_spacing=DET_SPACING,
         voxel_size=VOXEL_SIZE,
-        src_up=SRC_UP,
-        src_right=SRC_RIGHT,
+        det_col=DET_COL,
+        det_row=DET_ROW,
         filename_prefix="proj",
         phantom_name=PHANTOM_NAME
     )

phantom_reconstruction.py
@@ -51,14 +51,10 @@ def crop_with_bbox(img, bbox):
     y0, y1, x0, x1 = bbox
     return img[..., y0:y1, x0:x1] if img.ndim==3 else img[y0:y1, x0:x1]
 
-
-
-
-
 # ----------------------------
 # Paths
 # ----------------------------
-input_dir = r'C:\Users\Flex Ray\Documents\JGM\AutoCalibration\2026-02-19_Beads_phantom\Scan1\recon'
+input_dir = r'C:\Users\Flex Ray\Documents\JGM\AutoCalibration\real_scans\2026-02-19_Beads_phantom\Scan1\recon'
 output_dir = input_dir.replace('recon', 'cropped')
 os.makedirs(output_dir, exist_ok=True)
 
@@ -115,7 +111,7 @@ print("Image shapes after cropping should all be the same:", cropped_stack.shape
 # ----------------------------
 # Step 8: Load all cropped images into a single stack and save as .npy
 # ----------------------------
-vol = np.stack([imread(im) for im in sorted(glob('C:\\Users\\Flex Ray\\Documents\\JGM\\AutoCalibration\\2026-02-19_Beads_phantom\\Scan1\\cropped\\*.tif'))])
+vol = np.stack([imread(im) for im in sorted(glob('C:\\Users\\Flex Ray\\Documents\\JGM\\AutoCalibration\\real_scans\\2026-02-19_Beads_phantom\\Scan1\\cropped\\*.tif'))])
 
 # To Visualize in Napari
 #napari.imshow(recon, axis_labels=("y", "z", "x"))
@@ -130,11 +126,24 @@ vol = np.stack([imread(im) for im in sorted(glob('C:\\Users\\Flex Ray\\Documents
 # vol = np.load("recon_cropped_scan_19.npy")              # shape should be (595, 285, 190)
 
 # ----------------------------
-# Step 9: Resize to target shape (Z, Y, X) = (498, 240, 160) to match size of the phantom
+# Step 9: Resize to target shape (Z, Y, X) = (498, 240, 160)
 # ----------------------------
-target_shape = (498, 240, 160)          # (Z, Y, X)
+target_shape = (498, 240, 160)  # (Z, Y, X)
 
-shrunk_vol = resize(vol, target_shape, order=1, mode="reflect", anti_aliasing=True, preserve_range=True).astype(vol.dtype)
+shrunk_vol = resize(
+    vol,
+    target_shape,
+    order=1,
+    mode="reflect",
+    anti_aliasing=True,
+    preserve_range=True
+).astype(vol.dtype)
 
-np.save("scan2_160x240x498.npy", shrunk_vol)
-print("Resized volume saved as scan2_160x240x498.npy with shape", shrunk_vol.shape)
+# ----------------------------
+# Step 10: Rotate volume 180° around Unity Y axis
+# ----------------------------
+shrunk_vol = shrunk_vol[ :, ::-1,:]
+
+# Save
+np.save(r"phantoms\scan2_160x240x498_transposed_rotY180.npy", shrunk_vol)
+print("Saved rotated volume with shape", shrunk_vol.shape)